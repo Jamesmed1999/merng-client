f992283935b200f9e9693803929daee1c6b14b46
Jamesmed1999 <Jamesmed1999@gmail.com>
2021-01-22 00:22:41 -0500

    npm installed senatic ui react

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 import { Button, Confirm, Icon, Popup } from 'semantic-ui-react';
 
-import { FETCH_POSTS_QUERY } from '../util/graphql';
+/* import { FETCH_POSTS_QUERY } from '../util/graphql'; */
 
 function DeleteButton({ postId, commentId, callback }) {
   const [confirmOpen, setConfirmOpen] = useState(false);
@@ -11,7 +11,7 @@ function DeleteButton({ postId, commentId, callback }) {
   const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
 
   const [deletePostOrMutation] = useMutation(mutation, {
-    update(proxy) {
+    update() { //proxy
       setConfirmOpen(false);
    /*  does not work with apollo v3  if (!commentId) {
         const data = proxy.readQuery({
